921059a9604441623801a9222d6737d8335afe66
Alexander Azzo <azzoa@vcu.edu>
2018-12-28 15:55:08 -0500

    linux specfic stuff

--- linux_install_links.sh
+++ linux_install_links.sh
@@ -2,3 +2,6 @@
 
 ln -si $(pwd)/vim/vimrc ~/.vimrc
 ln -si $(pwd)/tmux/tmux.conf ~/.tmux.conf
+ln -si $(pwd)/bash/bashrc ~/.bashrc
+ln -si $(pwd)/bash/bash_profile ~/.bash_profile
+ln -si $(pwd)/bash/bash_aliases ~/.bash_aliases
